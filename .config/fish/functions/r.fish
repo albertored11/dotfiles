23974e2364f9f488f725c0b421d7ff861642a001
Alberto Redondo <albertomost@gmail.com>
2021-05-09 13:42:29 +0200

    r alias for ranger now cds on exit

--- .config/fish/functions/r.fish
+++ .config/fish/functions/r.fish
@@ -1,4 +1,4 @@
-# Defined in - @ line 1
-function r --wraps=ranger --description 'alias r=ranger'
-  ranger  $argv;
+# Defined via `source`
+function r --wraps=ranger --wraps=bash\ -c\ \'.\ ranger\;\ pwd\ \>\ /tmp/rcd\'\;\ cd\ \(cat\ /tmp/rcd\) --description alias\ r=bash\ -c\ \'.\ ranger\;\ pwd\ \>\ /tmp/rcd\'\;\ cd\ \(cat\ /tmp/rcd\)
+  bash -c '. ranger; pwd > /tmp/rcd'; cd (cat /tmp/rcd) $argv; 
 end
