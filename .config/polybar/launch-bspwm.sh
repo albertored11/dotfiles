647d13c00ff8e8df4ca49ff8ccc2ed987060082b
Alberto Redondo <albertomost@gmail.com>
2021-06-05 14:23:46 +0200

    Update i3 config; new bspwm module for polybar that shows number of hidden windows; add bspwm script for swaping focused node with hidden node; add bspc-swap-tab script; add i3workspace scripts

--- .config/polybar/launch-bspwm.sh
+++ .config/polybar/launch-bspwm.sh
@@ -6,6 +6,15 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Terminate bspwm-node-msg instances
+for p in $(pgrep -f bspwm-node-msg); do kill $p; done
+
+# Wait until the processes have been shut down
+while pgrep -f bspwm-node-msg >/dev/null; do sleep 1; done
+
+# Launch bspwm-node-msg
+$HOME/.config/polybar/bspwm-node-msg.sh &
+
 # Launch Polybar, using default config location ~/.config/polybar/config
 polybar main-bspwm &
 polybar clock-bspwm &
