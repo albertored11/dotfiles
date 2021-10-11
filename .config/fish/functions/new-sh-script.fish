f73615bbf99086838f74762e6199bef281fd424a
Alberto Redondo <albertomost@gmail.com>
2021-10-11 17:59:02 +0200

    Update fish function: new-sh-script

--- .config/fish/functions/new-sh-script.fish
+++ .config/fish/functions/new-sh-script.fish
@@ -1,4 +1,8 @@
 function new-sh-script
-echo "#!/usr/bin/env bash" > $argv[1]
-chmod +x $argv[1]
+    if test (count $argv) -eq 1
+        echo "#!/usr/bin/env bash" > $argv[1] && chmod +x $argv[1]
+    else
+        echo "Exactly 1 argument is needed" >&2
+        return 1
+    end
 end
