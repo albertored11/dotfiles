28c4cd107368faca3ca9ecff37d0e39ab1023ef5
Alberto Redondo <albertomost@gmail.com>
2021-10-11 17:42:17 +0200

    Little style changes to several files

--- .config/polybar/bspwm-hidden-local.sh
+++ .config/polybar/bspwm-hidden-local.sh
@@ -2,9 +2,3 @@
 
 count=$(bspc query -N -n .hidden.local.window | wc -l) 
 [[ $count == "0" ]] && echo || echo "[$count]"
-
-#while true; do
-#    count=$(bspc query -N -n .hidden.local.window | wc -l)
-#    [[ $count == "0" ]] && echo || echo "[$count]"
-#    sleep 0.2
-#done
