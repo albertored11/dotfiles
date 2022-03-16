da5de1a3bd6bde3765a8ffba43b23af8bbb91de8
Alberto Redondo <albertomost@gmail.com>
2022-03-16 20:50:02 +0100

    config.fish: add conda initialize

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,3 +6,9 @@ if status is-login
         exec startx -- -keeptty &>/dev/null
     end
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/herbort/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
